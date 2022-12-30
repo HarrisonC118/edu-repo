82ab79240bb609b412ce3c49b52995eb0f05d103
hatcherCheung <1109079622@qq.com>
2022-12-30 17:52:24 +0800

    配置eureka

---
+++ edu-eureka-boot/src/main/java/com/hatcher/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hatcher
+ * @date 2022/12/30 17:29
+ * @description EurekaApplication
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
