01977f7217d133c6a71d073160baa55c540a95da
hatcherCheung <1109079622@qq.com>
2022-12-30 21:50:41 +0800

    完成配置中心

---
+++ edu-config-boot/src/main/java/com/hatcher/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.hatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author hatcher
+ * @date 2022/12/30 17:57
+ * @description
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
+
