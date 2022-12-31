2980ce896bc17a9db37dfd6c4747c4d18bd07aba
hatcherCheung <1109079622@qq.com>
2022-12-31 10:37:13 +0800

    完成网关配置

---
+++ edu-gateway-boot/src/main/java/com/hatcher/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.hatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hatcher
+ * @date 2022/12/31 10:33
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
